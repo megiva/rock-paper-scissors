0fd2433b6190c3eae6bf62ff645219807d29b8e0
megiva <meghana.m.ivaturi@gmail.com>
2024-08-01 10:36:22 -0400

    Create playRound, checkGameOver, and playGame logic

--- script.js
+++ script.js
@@ -36,4 +36,59 @@ score.style.color = "purple";
 score.textContent = '';
 header.textContent = '';
 
+function playRound(humanChoice) {
+    header.textContent = "ROUND " + numRound++;
+ 
+
+    let computerChoice = getComputerChoice();
+    if (computerChoice === humanChoice){
+        let result = "It's a tie! " + computerChoice + " equals " + humanChoice;
+        words.textContent = result;
+    }
+
+    else if (computerChoice === "rock" && humanChoice === "scissors" ||
+        computerChoice === "scissors" && humanChoice === "paper" ||
+        computerChoice === "paper" && humanChoice === "rock"){
+        computerScore++;
+        let result = computerChoice + " beats " + humanChoice + ", You lose!";
+        words.textContent = result;
+
+    }
+    else {
+        humanScore++;
+        let result = humanChoice + " beats " + computerChoice + ", You win!";
+        words.textContent = result;
+    } 
+    score.textContent = "Human score: " + humanScore + " Computer score: " + computerScore;
+
+    checkGameOver();
+}
+
+function checkGameOver() {
+    if (humanScore >= 5 || computerScore >= 5) {
+        if (humanScore > computerScore) {
+            words.textContent = "YOU WIN!";
+        } else if (humanScore === computerScore) {
+            words.textContent = "IT'S A DRAW!";
+        } else {
+            words.textContent = "YOU LOSE!";
+        }
+
+        // Reset scores for a new game
+        humanScore = 0;
+        computerScore = 0;
+        numRound = 1;
+    }
+
+}
+        
+
+function playGame() { 
+    header.textContent = "The game is starting! First to five wins!";
+}
+
+playGame();
+
+
+
 
