82554aefb35b7681c1dc3be9a54988b6e4c8fdd0
megiva <meghana.m.ivaturi@gmail.com>
2024-08-01 10:34:25 -0400

    Make getComputerChoice and create HTML elements

--- script.js
+++ script.js
@@ -12,4 +12,28 @@ scissorsBtn.addEventListener('click', () => {
     playRound('scissors');
 });
 
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*3);
+    if (computerChoice == 0) return 'rock'
+    if (computerChoice == 1) return 'paper'
+    if (computerChoice == 2) return 'scissors'
+}
+
+let humanScore = 0;
+let computerScore = 0; 
+let numRound = 1;
+
+const content = document.getElementById("content");
+const header = document.createElement("h2");
+header.style.color = "blue";
+content.appendChild(header);
+const score = document.createElement("h6");
+const words = document.createElement("h4")
+content.appendChild(words);
+content.appendChild(score);
+words.style.color = "magenta";
+score.style.color = "purple";
+score.textContent = '';
+header.textContent = '';
+
 
